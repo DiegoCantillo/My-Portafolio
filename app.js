20d871910b7052f07d7cdab8fd3ec2f94003bd29
Luis Diego Cantillo <109639390+DiegoCantillo@users.noreply.github.com>
2023-09-19 19:00:08 -0500

    portafolio actualizado

--- app.js
+++ app.js
@@ -2,17 +2,28 @@ const menu = document.querySelector(".menu");
 const navBar = document.querySelector(".navigation");
 const close = document.querySelector(".close");
 
-console.log(navBar);
+const span = document.getElementById("text-animation");
 
-menu.addEventListener("click", ()=> {
+
+menu.addEventListener("click", () => {
     navBar.style.display = "block";
     menu.style.display = "none";
     close.style.display = "block"
 })
 
 
-close.addEventListener("click", ()=>{
+close.addEventListener("click", () => {
     navBar.style.display = "none";
     menu.style.display = "block";
     close.style.display = "none"
 })
+
+function startAnimation() {
+    span.style.animation = "none";
+    setTimeout(() => {
+        span.style.animation = "typing 3s steps(18) forwards";
+
+        setTimeout(startAnimation, 3000);
+    }, 3000);
+}
+startAnimation();
